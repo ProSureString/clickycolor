3fb846f7d0a3f8b0f27b07b9f52d0fb18b0d86c3
ProSureString <prosurestring@prosurestring.xyz>
2025-10-26 20:49:33 -0700

    TEST continuiys

--- clickycolor/clickycolor.cpp
+++ clickycolor/clickycolor.cpp
@@ -1,11 +1,19 @@
 #include <Windows.h>
 #include <iostream>
 
-COLORREF GetPixelColor(int x, int y)
+HDC GetScreenDC()
 {
-	HDC hdcScreen = GetDC(NULL);
-	COLORREF color = GetPixel(hdcScreen, x, y);
-	ReleaseDC(NULL, hdcScreen);
+	return GetDC(NULL);
+}
+
+void ReleeaseScreenDC(HDC hdc)
+{
+	ReleaseDC(NULL, hdc);
+}
+
+COLORREF GetPixelColor(int x, int y, HDC hdc)
+{
+	COLORREF color = GetPixel(hdc, x, y);
 
 	return color;
 }
@@ -15,7 +23,14 @@ int main()
 	POINT p;
 	GetCursorPos(&p);
  
-	COLORREF c = GetPixelColor(p.x, p.y);
+	HDC hdc = GetScreenDC();
 
-	std::cout << "Pixel at (" << p.x << ", " << p.y << ") -> " << "R: " << (int)GetRValue(c) << " G: " << (int)GetGValue(c) << " B: " << (int)GetBValue(c) << std::endl;
+	while (true) {
+		COLORREF c = GetPixelColor(p.x, p.y, hdc);
+		std::cout << "Pixel at (" << p.x << ", " << p.y << ") -> " << "R: " << (int)GetRValue(c) << " G: " << (int)GetGValue(c) << " B: " << (int)GetBValue(c) << std::endl;
+	}
+
+
+	ReleeaseScreenDC(hdc);
+
 }
