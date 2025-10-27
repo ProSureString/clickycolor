d34104f83f70b82ebe58ec00657dd280cfd77eec
ProSureString <prosurestring@prosurestring.xyz>
2025-10-26 20:46:30 -0700

    get color

--- clickycolor/clickycolor.cpp
+++ clickycolor/clickycolor.cpp
@@ -1,7 +1,21 @@
-
+#include <Windows.h>
 #include <iostream>
+
+COLORREF GetPixelColor(int x, int y)
+{
+	HDC hdcScreen = GetDC(NULL);
+	COLORREF color = GetPixel(hdcScreen, x, y);
+	ReleaseDC(NULL, hdcScreen);
+
+	return color;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	POINT p;
+	GetCursorPos(&p);
+ 
+	COLORREF c = GetPixelColor(p.x, p.y);
+
+	std::cout << "Pixel at (" << p.x << ", " << p.y << ") -> " << "R: " << (int)GetRValue(c) << " G: " << (int)GetGValue(c) << " B: " << (int)GetBValue(c) << std::endl;
 }
