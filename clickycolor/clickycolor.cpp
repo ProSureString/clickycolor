b7af43196d10fff3c4b558a5a66275e478da9cf8
ProSureString <prosurestring@prosurestring.xyz>
2025-10-26 21:27:26 -0700

    fix color function

--- clickycolor/clickycolor.cpp
+++ clickycolor/clickycolor.cpp
@@ -7,16 +7,24 @@ bool isWithinRange(int value, int target, int range)
 	return std::abs(value - target) <= range;
 }
 
-bool isYellowOrOrange(COLORREF c, int to1Yellow = 50, int to1Orange = 25) 
+bool isYellowOrOrange(COLORREF c) 
 {
 	int r = GetRValue(c);
 	int g = GetGValue(c);
 	int b = GetBValue(c);
 
-	bool isYellow = isWithinRange(r, 255, to1Yellow) && isWithinRange(g, 255, to1Yellow) && isWithinRange(b, 0, to1Yellow);
-	bool isOrange = isWithinRange(r, 255, to1Orange) && isWithinRange(g, 165, to1Orange) && isWithinRange(b, 0, to1Orange);
+	int brightness = (r + g + b) / 3;
 
-	return isYellow || isOrange;
+	bool warmTone(r > g * 0.8 && r > b * 1.2);
+
+
+	bool yellowish = (r > 180 && g > 140 && b < 120);
+	bool orangish = (r > 120 && g > 60 && b < 80);
+
+
+	bool darkYellow = (brightness > 40 && brightness < 140 && r > g && g > b);
+
+	return (warmTone && (yellowish || orangish || darkYellow));
 }
 
 COLORREF GetPixelColor(int x, int y)
